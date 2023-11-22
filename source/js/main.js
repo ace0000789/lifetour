d40fa2dba75400f9bc1189d2ddb83531c8bbbd0c
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-11-22 12:31:35 +0100

    add webp and first img for slider

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {initMobileMunu} from './init-mobile-menu';
+import {initPromoSwiper} from './init-promo-swiper';
 
 // ---------------------------------
 
@@ -15,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   initMobileMunu();
-
+  initPromoSwiper();
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
