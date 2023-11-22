1314c2835032a747a0f526e52c1d63158299dda5
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-11-22 17:51:23 +0100

    ыдшвук

--- source/js/init-promo-swiper.js
+++ source/js/init-promo-swiper.js
@@ -4,6 +4,61 @@ import Swiper from './vendor/swiper';
 function initPromoSwiper() {
   const myImageSlider = new Swiper('[data-validate="swiper-container"]', {
 
+
+    // Пагинация
+    pagination: {
+      el: '.swiper-pagination',
+
+      // Булеты
+      clickable: true,
+    },
+
+
+
+    // перетаскивание на ПК
+    simulateTouch: true,
+    // чувствительность свайпа
+    touchRatio: 1,
+    // угол срабатывания свайпа
+    touchAngle: 45,
+    // курсор перетаскивания
+    grabCursor: true,
+
+    // Управление клавиатурой
+    keyboard: {
+      // вкл/выкл
+      enbled: true,
+      // вкл/выкл только когда слайдер в пределах вьюпорта
+      onlyInViewport: true,
+    },
+
+    // Автовысота картинки
+    autoHeight: true,
+    // Бесконечный слайдер
+    loop: false,
+    // Количнство дублирущих слайдов
+    loopedSlides: 0,
+    // Свободный режим (перетаскивание по 1 фрагменту)
+    freeMod: true,
+    // Скорость переклдючения слайда
+    speed: 500,
+    // Листание
+    effect: 'slide',
+    breakpoints: {
+      1440: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        allowTouchMove: true,
+      },
+      782: {
+        slidesPerView: 1,
+      },
+
+      0: {
+        slidesPerView: 1,
+      },
+    },
   });
   return myImageSlider;
 }
