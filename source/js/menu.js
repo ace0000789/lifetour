c997b64de1b930a58fa8f98f670a340f9ee46a12
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-11-22 11:00:54 +0100

    add bg for wrapper if menu open

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,7 @@ export function initHeaderToggle() {
 
   let header = document.querySelector('[data-validate="header"]');
   let headerToggle = document.querySelector('[data-validate="toggle"]');
+  let wrapper = document.querySelector('[data-validate="wrapper"]');
 
   // header.removeAttribute('class');  убрать класс вместо remove
 
@@ -11,9 +12,11 @@ export function initHeaderToggle() {
     if (header.classList.contains('is--closed')) {
       header.classList.remove('is--closed');
       header.classList.add('is--opened');
+      wrapper.classList.add('is--bg');
     } else {
       header.classList.add('is--closed');
       header.classList.remove('is--opened');
+      wrapper.classList.remove('is--bg');
     }
   }
 }
