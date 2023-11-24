64f7dc6af6c19d34285888f925673fc732e98a7d
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-11-24 17:39:36 +0100

    ц

--- source/js/init-mobile-menu.js
+++ source/js/init-mobile-menu.js
@@ -1,12 +1,12 @@
 export function initMobileMunu() {
   let header = document.querySelector('[data-validate="header"]');
   let headerToggle = document.querySelector('[data-validate="toggle"]');
-  let wrapper = document.querySelector('[data-validate="wrapper"]');
+  //let wrapper = document.querySelector('[data-validate="wrapper"]');
 
   header.classList.remove('is-nojs');
 
-  // проверка на наличие эдементов
-  if (!header || !headerToggle || !wrapper) {
+  // проверка на наличие элементов
+  if (!header || !headerToggle ) {
     return;
   }
 
@@ -16,11 +16,11 @@ export function initMobileMunu() {
     if (header.classList.contains('is-closed')) {
       header.classList.remove('is-closed');
       header.classList.add('is-opened');
-      wrapper.classList.add('is-bg');
+      //wrapper.classList.add('is-bg');
     } else {
       header.classList.add('is-closed');
       header.classList.remove('is-opened');
-      wrapper.classList.remove('is-bg');
+      //wrapper.classList.remove('is-bg');
     }
   }
 }
