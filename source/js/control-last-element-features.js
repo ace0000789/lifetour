8f83e5524a00f540f9288dcaa80811b9c9365eda
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-12-05 17:33:37 +0100

    add event for function last element features

--- source/js/control-last-element-features.js
+++ source/js/control-last-element-features.js
@@ -1,15 +1,23 @@
 export const controlLastELementFeatures = () => {
 
-  const featuresItems = document.querySelectorAll('.features .swiper-slide');
+  function checkLastElement() {
+    const featuresItems = document.querySelectorAll('.features .swiper-slide');
 
-  if (!featuresItems) {
-    return;
+    if (!featuresItems) {
+      return;
+    }
+
+    const isOdd = featuresItems.length % 2 !== 0;
+    if (isOdd) {
+      const lastElement = featuresItems[featuresItems.length - 1];
+      lastElement.classList.add('center-last');
+    }
   }
 
-  const isOdd = featuresItems.length % 2 !== 0;
-  if (isOdd) {
-    const lastElement = featuresItems[featuresItems.length - 1];
-    lastElement.classList.add('center-last');
-  }
+  checkLastElement();
+
+  window.addEventListener('resize', () => {
+    checkLastElement();
+  });
+
 };
-
