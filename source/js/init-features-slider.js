45919a6dcee79b70aeedde8dd744a62499215331
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-12-05 12:28:45 +0100

    js check for width 1199 features

--- source/js/init-features-slider.js
+++ source/js/init-features-slider.js
@@ -1,57 +1,75 @@
 import Swiper from './vendor/swiper';
 
 function initFeaturesSwiper() {
-  const myImageSlider = new Swiper('[data-validate="swiper-features"]', {
+  let swiper = null;
 
-    navigation: {
-      nextEl: '[data-validate="features-button-next"]',
-      prevEl: '[data-validate="features-button-prev"]',
-    },
+  function init() {
+    swiper = new Swiper('[data-validate="swiper-features"]', {
+      // общие опции
 
-    // перетаскивание на ПК
-    simulateTouch: true,
-    // чувствительность свайпа
-    touchRatio: 1,
-    // угол срабатывания свайпа
-    touchAngle: 45,
-    // курсор перетаскивания
-    grabCursor: true,
-
-    // Управление клавиатурой
-    keyboard: {
-      // вкл/выкл
-      enbled: true,
-      // вкл/выкл только когда слайдер в пределах вьюпорта
-      onlyInViewport: true,
-    },
-
-    // Автовысота картинки
-    autoHeight: true,
-    // Бесконечный слайдер
-    loop: true,
-    // Количнство дублирущих слайдов
-    loopedSlides: 0,
-    // Свободный режим (перетаскивание по 1 фрагменту)
-    freeMod: true,
-    // Скорость переклдючения слайда
-    speed: 500,
-    // Листание
-    effect: 'slide',
-    breakpoints: {
-      1200: {
-        slidesPerView: 'auto',
-        slidesPerGroup: 1,
-        centeredSlides: true,
-        allowTouchMove: true,
-        spaceBetween: 30,
-        watchSlidesProgress: true,
-        initialSlide: 3,
+      navigation: {
+        nextEl: '[data-validate="features-button-next"]',
+        prevEl: '[data-validate="features-button-prev"]',
       },
-      1199: {
-        enabled: false,
+
+      // перетаскивание на ПК
+      simulateTouch: true,
+      // чувствительность свайпа
+      touchRatio: 1,
+      // угол срабатывания свайпа
+      touchAngle: 45,
+      // курсор перетаскивания
+      grabCursor: true,
+
+      // Управление клавиатурой
+      keyboard: {
+        // вкл/выкл
+        enbled: true,
+        // вкл/выкл только когда слайдер в пределах вьюпорта
+        onlyInViewport: true,
       },
-    },
-  });
-  return myImageSlider;
+
+      // Автовысота картинки
+      autoHeight: true,
+      // Бесконечный слайдер
+      loop: true,
+      // Количнство дублирущих слайдов
+      loopedSlides: 0,
+      // Свободный режим (перетаскивание по 1 фрагменту)
+      freeMod: true,
+      // Скорость переклдючения слайда
+      speed: 500,
+      // Листание
+      effect: 'slide',
+      breakpoints: {
+        // брейкпойнты
+
+        1200: {
+          slidesPerView: 'auto',
+          slidesPerGroup: 1,
+          centeredSlides: true,
+          allowTouchMove: true,
+          spaceBetween: 30,
+          watchSlidesProgress: true,
+          initialSlide: 3,
+        },
+      },
+    });
+  }
+
+  init();
+
+  function handleResize() {
+    if (window.innerWidth < 1200) {
+      swiper.destroy(true, true);
+      swiper = null;
+    } else if (!swiper) {
+      init();
+    }
+  }
+
+  window.addEventListener('resize', handleResize);
+
 }
+
 export {initFeaturesSwiper};
