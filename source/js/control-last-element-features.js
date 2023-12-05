6ab57f04d22223085558976f2cab9e3ed4867ff5
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-12-05 15:21:32 +0100

    js check last features item ok

--- source/js/control-last-element-features.js
+++ source/js/control-last-element-features.js
@@ -1,15 +1,10 @@
-export function controlLastELementFeatures() {
+export const controlLastELementFeatures = () => {
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const featuresItems = document.querySelectorAll('data-validate="swiper-features" .swiper-slide');
-    const isOdd = featuresItems.length % 2 !== 0;
+  const featuresItems = document.querySelectorAll('.features .swiper-slide');
+  const isOdd = featuresItems.length % 2 !== 0;
+  if (isOdd) {
+    const lastElement = featuresItems[featuresItems.length - 1];
+    lastElement.classList.add('center-last');
+  }
+};
 
-    if (isOdd) {
-      const lastElement = featuresItems[featuresItems.length - 1];
-      lastElement.classList.add('center-last');
-    }
-  });
-}
-
-
-/**/
